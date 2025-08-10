ac438fac66797e19cdab6aa2343fb45492c5ac31
Sana Afreen Yafai <Sana.Afreen.Yafai@kyndryl.com>
2025-08-10 20:38:44 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Document Q&A API')
-    .setDescription('API for uploading, ingesting, and querying documents')
-    .setVersion('1.0')
-    .addBearerAuth() // for JWT support
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
