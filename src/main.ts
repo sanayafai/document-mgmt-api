79486b2669036ab184875e984b87ff63a567179d
Sana Afreen Yafai <Sana.Afreen.Yafai@kyndryl.com>
2025-08-10 21:14:39 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  });
+  // Optional: if you have a global prefix, e.g. 'api'
+  // app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+      .setTitle('API Docs')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Make sure the path here matches the URL you are visiting:
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
